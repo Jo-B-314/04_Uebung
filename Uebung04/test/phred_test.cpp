b7c4d9d401b1c39ceba5bf85ff6c28851d4cad16
Jo-B-314 <s8jobech@stud.uni-saarland.de>
2020-11-30 15:51:26 +0100

    test writer

--- Uebung04/test/phred_test.cpp
+++ Uebung04/test/phred_test.cpp
@@ -10,4 +10,28 @@
 
 #include "../Sequence.h"
 #include "../DNA.h"
+#include "../Fastq.h"
 
+using namespace Alphabet;
+
+TEST(Fastq, writer)
+{
+    Sequence<DNA> Dnaseq;
+    Dnaseq.push_back(DNA::Characters::T);
+    Dnaseq.push_back(DNA::Characters::A);
+    Dnaseq.push_back(DNA::Characters::C);
+    Dnaseq.setComment("test");
+    Sequence<DNA>& seq = Dnaseq;
+    const Sequence<DNA> cseq = seq;
+    SequenceFastqWriter<DNA> writer (cseq);
+    auto qval = seq.getQValues();
+    std::string qvalstr;
+    for (int i = 0; i < qval.size(); i++) {
+        qvalstr.push_back(qval[i]);
+    }
+    std::string str = "@test\n" + seq.toString() + "\n" + "+\n" + qvalstr ;
+    std::stringstream ss;
+    ss << writer;
+    ASSERT_EQ(0, str.compare(ss.str()));
+
+}
